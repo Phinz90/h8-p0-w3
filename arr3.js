be5cefaa8279f177984b44fc8d737c0aef52fc38
Yosephine Mellyana <42544640+Phinz90@users.noreply.github.com>
2018-09-09 12:54:47 +0700

    Add files via upload

---
+++ arr3.js
@@ -0,0 +1,31 @@
+// function dataHandling2(data) 
+
+var input = [
+  '0001', 
+  'Roman Alamsyah',
+  'Bandar Lampung',
+  '21/05/1989',
+  'Pria',
+  'Membaca',
+];
+
+function dataHandling2(inp) {
+ console.log(inp)
+var tanggal = '21/05/1989'
+var kata = tanggal.split ('/') 
+console.log (kata) 
+
+}
+
+dataHandling2(input)
+// dataHandling2.splice (1, 1, 'Roman Alamsyah Elsharawy');
+// dataHandling2.splice (2, 1, 'Provinsi Bandar Lampung');
+// dataHandling2.splice (4, 0, 'Pria');
+// dataHandling2.splice (5, 1, 'SMA Internasional Metro');
+// console.log (dataHandling2);
+
+// ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+
+// var 
+
+
